46201b730c64b9f691a30c506607bf46b747d5be
iolanda.rosa <iolanda.rosa777@gmail.com>
2019-10-08 21:12:31 +0100

    Commit without node modules

---
+++ app/Http/Controllers/LoginControllerAPI.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Response;
+use App\User;
+
+define('YOUR_SERVER_URL', 'http://projet.dad');
+// Check "oauth_clients" table for next 2 values:
+define('CLIENT_ID', '2');
+define('CLIENT_SECRET','oOE2JbBlAUkRI9AkYnOUyAa3GWdY3Ad7fkfk64rU');
+
+class LoginControllerAPI extends Controller
+{
+
+	public function login(Request $request)
+	{
+		$http = new \GuzzleHttp\Client;
+		$response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
+			'form_params' => [
+				'grant_type' => 'password',
+				'client_id' => CLIENT_ID,
+				'client_secret' => CLIENT_SECRET,
+				'username' => $request->email,
+				'password' => $request->password,
+				'scope' => ''
+			],
+			'exceptions' => false,
+		]);
+		$errorCode= $response->getStatusCode();
+		if ($errorCode=='200') {
+			return json_decode((string) $response->getBody(), true);
+		} else {
+			return response()->json(['msg'=>'User credentials are invalid'], $errorCode);
+		}
+	}
+
+	public function loginUsername(Request $request){
+		$http = new \GuzzleHttp\Client;
+		
+		$user = User::where('username', '=', $request->input('username'))->get();
+
+		$response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
+			'form_params' => [
+				'grant_type' => 'password',
+				'client_id' => CLIENT_ID,
+				'client_secret' => CLIENT_SECRET,
+				'username' => $user[0]->email,
+				'password' => $request->password,
+				'scope' => ''
+			],
+			'exceptions' => false,
+		]);
+		$errorCode= $response->getStatusCode();
+		if ($errorCode=='200') {
+			return json_decode((string) $response->getBody(), true);
+		} else {
+			return response()->json(['msg'=>'User credentials are invalid'], $errorCode);
+		}
+	}
+
+	public function logout()
+	{
+		\Auth::guard('api')->user()->token()->revoke();
+		\Auth::guard('api')->user()->token()->delete();
+		return response()->json(['msg'=>'Token revoked'], 200);
+	}
+}
